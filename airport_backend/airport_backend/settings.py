c83f2eba52596e16e99568cb9c546d80f48b2723
justwoork <157926518+justwoork@users.noreply.github.com>
2024-11-03 15:44:30 +0300

    Create django apps, change database to PostgresSQL

--- airport_backend/airport_backend/settings.py
+++ airport_backend/airport_backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'airport_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'airport_database',
+        'USER': 'postgres',
+        'PASSWORD': 'ab3376803',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
