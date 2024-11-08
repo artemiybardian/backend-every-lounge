0a1030e422366cd7249bcebbf546e01e5f3a8f22
Artemiy <artemibardian@gmail.com>
2024-11-08 11:28:58 +0000

    Some fixes on server

--- airport_backend/airport_backend/urls.py
+++ airport_backend/airport_backend/urls.py
@@ -21,8 +21,6 @@ schema_view = get_schema_view(
 from django.urls import path, include
 
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger',
-         cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', include('admin_panel.urls')),
     path('api/users/', include('users.urls')),
     path('api/locations/', include('locations.urls')),
