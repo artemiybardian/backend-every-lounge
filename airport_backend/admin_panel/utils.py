ee67cd687c863b276c051770f00c58f7488084be
justwoork <157926518+justwoork@users.noreply.github.com>
2024-11-08 19:45:22 +0300

    some fix

--- airport_backend/admin_panel/utils.py
+++ airport_backend/admin_panel/utils.py
@@ -12,14 +12,6 @@ def log_admin_action(admin_user, action_description):
 
 
 def send_telegram_notification(user_telegram_id, message):
-    try:
-        # Убедитесь, что это целое число
-        user_telegram_id = int(user_telegram_id)
-    except ValueError:
-        logging.error(
-            "Некорректный формат user_telegram_id: не является числом")
-        return
-
     token = '7702184372:AAFpYNtn0V1MJRYB7BwgYVkd1pBhdWvSXCU'
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     data = {
