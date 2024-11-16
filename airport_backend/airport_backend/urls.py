ecba48e7fdc0d89d261f221825572c2e48c4f6c1
justwoork <157926518+justwoork@users.noreply.github.com>
2024-11-16 17:43:52 +0300

    fix

--- airport_backend/airport_backend/urls.py
+++ airport_backend/airport_backend/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.schemas import get_schema_view
 from django.views.generic import TemplateView
 
@@ -21,3 +23,7 @@ urlpatterns = [
     path('api/payments/', include('payments.urls')),
     path('api/notifications/', include('notifications.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
