f3cfcd26625a97009f168a602877b7aefc0558d7
justwoork <157926518+justwoork@users.noreply.github.com>
2024-11-17 13:08:00 +0300

    fix

--- airport_backend/locations/urls.py
+++ airport_backend/locations/urls.py
@@ -6,9 +6,10 @@ from locations import views
 router = SimpleRouter()
 router.register(r'airports', views.AirportReadOnlyViewSet)
 router.register(r'lounges', views.LoungeReadOnlyViewSet, basename='lounge')
-router.register(r'nearest_airports', views.NearestAirportsViewSet,
-                basename='nearest_airports')
+# router.register(r'nearest_airports', views.NearestAirportsViewSet,
+#                 basename='nearest_airports')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('nearest_airports/', views.NearestAirportsView.as_view())
 ]
