2994fb7fb41734d1045054e4a4215613b9371056
justwoork <157926518+justwoork@users.noreply.github.com>
2024-11-18 14:56:29 +0300

    feature

--- airport_backend/telegram_bot/tbot.py
+++ airport_backend/telegram_bot/tbot.py
@@ -1,7 +1,10 @@
 import telebot
-from telebot.types import (ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo)
+from telebot.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo)
 import requests
 import logging
+from datetime import datetime, timedelta
+import threading
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -11,8 +14,39 @@ API_TOKEN = '7702184372:AAFpYNtn0V1MJRYB7BwgYVkd1pBhdWvSXCU'
 DJANGO_API_URL = 'https://lounge-booking.com/api/users/auth/'
 WEBAPP_URL = 'https://lounge-booking.com/'
 
+# Хранилище кнопок
+buttons_store = {}
 
 bot = telebot.TeleBot(API_TOKEN)
+
+# Фоновая задача для удаления устаревших кнопок
+
+
+def cleanup_old_buttons():
+    while True:
+        now = datetime.now()
+        to_delete = []
+
+        for key, value in buttons_store.items():
+            if now - value['timestamp'] > timedelta(days=1):
+                try:
+                    # Удаление кнопки из сообщения
+                    bot.edit_message_reply_markup(
+                        chat_id=value['chat_id'], message_id=value['message_id'], reply_markup=None)
+                    to_delete.append(key)
+                except Exception as e:
+                    logger.error(f"Ошибка удаления кнопки: {e}")
+
+        # Удаление из хранилища
+        for key in to_delete:
+            del buttons_store[key]
+
+        # Проверяем каждые 10 минут
+        threading.Event().wait(600)
+
+
+# Запускаем фоновую задачу
+threading.Thread(target=cleanup_old_buttons, daemon=True).start()
 
 # Обработка команды /start
 
@@ -63,29 +97,40 @@ def handle_location(message):
 
         if response.status_code == 200:
             bot.send_message(message.chat.id, "Ваши данные успешно обновлены.")
-            
+
             # Получаем токен доступа из Django API
             response_data = response.json()
             if 'token' in response_data:
                 access_token = response_data['token']
-                
+
                 # Создаем кнопку для перехода в WebApp с передачей токена
                 keyboard = InlineKeyboardMarkup()
                 button = InlineKeyboardButton(
-                    text="Перейти в WebApp", 
+                    text="Перейти в WebApp",
                     web_app=WebAppInfo(f"{WEBAPP_URL}?token={access_token}")
                 )
                 keyboard.add(button)
 
-                bot.send_message(
+                # Отправляем сообщение с кнопкой
+                sent_message = bot.send_message(
                     message.chat.id,
                     "Вы можете перейти в ваш аккаунт в WebApp, нажав на кнопку ниже:",
                     reply_markup=keyboard
                 )
+
+                # Сохраняем информацию о кнопке
+                buttons_store[sent_message.message_id] = {
+                    'chat_id': sent_message.chat.id,
+                    'message_id': sent_message.message_id,
+                    'timestamp': datetime.now()
+                }
             else:
-                bot.send_message(message.chat.id, "Не удалось получить токен. Попробуйте еще раз.")
+                bot.send_message(
+                    message.chat.id, "Не удалось получить токен. Попробуйте еще раз.")
         else:
             bot.send_message(message.chat.id, "Ошибка при обновлении данных.")
+
+
 # Запуск бота
 if __name__ == '__main__':
     logger.info("Запуск бота...")
