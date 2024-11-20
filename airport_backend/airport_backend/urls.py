63a0d4a04af1d0e19d2e3e2a8a4472d5af883098
justwoork <157926518+justwoork@users.noreply.github.com>
2024-11-20 19:12:28 +0300

    fix

--- airport_backend/airport_backend/urls.py
+++ airport_backend/airport_backend/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path, include, re_path
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
-# from rest_framework.schemas import get_schema_view
-# from django.views.generic import TemplateView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -25,28 +21,14 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    # path('api/api_schema/', get_schema_view(
-    #     title='API Schema',
-    #     description='Guide for the REST API'
-    # ), name='api_schema'),
-    # path("api/swagger/", TemplateView.as_view(
-    #             template_name="swagger-ui.html",
-    #             extra_context={"schema_url": "/api/api_schema/"},
-    #     ),
-    #      name="swagger-ui",
-    # ),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger',
          cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
-    path('api/locations/', include('locations.urls')),
-    path('api/bookings/', include('bookings.urls')),
-    path('api/payments/', include('payments.urls')),
-    path('api/notifications/', include('notifications.urls')),
+    path('users/', include('users.urls')),
+    path('locations/', include('locations.urls')),
+    path('bookings/', include('bookings.urls')),
+    path('payments/', include('payments.urls')),
+    path('notifications/', include('notifications.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
